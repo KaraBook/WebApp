b96549b956fbcc6a2deb86ab245917d1b489fe38
SakshiBandichawde <sakshibandichawade@gmail.com>
2025-10-12 19:45:38 +0530

    redirect file added

--- client/src/ProtectedAdminRoute.jsx
+++ client/src/ProtectedAdminRoute.jsx
@@ -3,7 +3,6 @@ import { Navigate, Outlet } from "react-router-dom";
 function ProtectedAdminRoute() {
   const token = localStorage.getItem("accessToken");
 
-  // You can also decode and check token role here if needed
 
   return token ? <Outlet /> : <Navigate to="/admin/login" replace />;
 }
