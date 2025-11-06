e56300863f80cc88fe500b054b1337dc7ef15a05
SakshiBandichawde <sakshibandichawade@gmail.com>
2025-11-06 21:43:32 +0530

    update viw

--- owner/src/pages/ViewProperty.jsx
+++ owner/src/pages/ViewProperty.jsx
@@ -5,6 +5,7 @@ import SummaryApi from "../common/SummaryApi";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 import { Separator } from "@/components/ui/separator";
 import {
   Loader2,
