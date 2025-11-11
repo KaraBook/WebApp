df7f688fe9131dcb6e4217151ffaec243944fd47
SakshiBandichawde <sakshibandichawade@gmail.com>
2025-11-11 10:01:41 +0530

    updated toast msg

--- owner/src/main.jsx
+++ owner/src/main.jsx
@@ -4,12 +4,19 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "./auth/AuthContext.jsx";
+import { Toaster } from "sonner"; 
+import "sonner/dist/sonner.css"; 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter basename="/owner">
       <AuthProvider>
         <App />
+        <Toaster
+          position="top-center"  
+          richColors              
+          closeButton             
+        />
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
