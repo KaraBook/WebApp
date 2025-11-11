284b5995bc18967801fdbfcfc46afd64147514c1
SakshiBandichawde <sakshibandichawade@gmail.com>
2025-11-11 20:16:54 +0530

    updated file b

--- owner/src/pages/OfflineBooking.jsx
+++ owner/src/pages/OfflineBooking.jsx
@@ -6,7 +6,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Card, CardHeader, CardContent, CardTitle } from "@/components/ui/card";
 import { toast } from "sonner";
-import { loadRazorpay } from "../utils/Razorpay";
+import loadRazorpay from "../utils/Razorpay";
 import SummaryApi from "@/common/SummaryApi";
 import api from "../api/axios";
 
