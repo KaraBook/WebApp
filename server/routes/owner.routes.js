861aba91b0cdf75c093b8a07fa87e58d132a08e1
SakshiBandichawde <sakshibandichawade@gmail.com>
2025-11-11 15:19:34 +0530

    updated route file

--- server/routes/owner.routes.js
+++ server/routes/owner.routes.js
@@ -10,6 +10,7 @@ import {
   addBlockedDates,
   removeBlockedDates,
 } from "../controllers/owner.controller.js";
+import upload from "../middlewares/multer.js";
 
 const router = express.Router();
 
@@ -17,8 +18,16 @@ router.get("/dashboard", requireAuth, getOwnerDashboard);
 router.get("/my-properties", requireAuth, getOwnerProperties);
 router.get("/bookings", requireAuth, getOwnerBookings);
 router.get("/property/:id", requireAuth, getSingleOwnerProperty);
-router.put("/property/:id", requireAuth, updateOwnerProperty);
-
+router.put(
+  "/property/:id",
+  requireAuth,
+  upload.fields([
+    { name: "coverImage", maxCount: 1 },
+    { name: "shopAct", maxCount: 1 },
+    { name: "galleryPhotos", maxCount: 10 },
+  ]),
+  updateOwnerProperty
+);
 router.get("/property/:id/blocked-dates", requireAuth, getPropertyBlockedDates);
 router.post("/property/:id/block-dates", requireAuth, addBlockedDates);
 router.delete("/property/:id/block-dates", requireAuth, removeBlockedDates);
