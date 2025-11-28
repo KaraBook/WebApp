ebe031a6d068d0d8a8acaf160bddad8f58ecc05c
SakshiBandichawde <sakshibandichawade@gmail.com>
2025-11-28 11:49:30 +0530

    updated file

--- owner/src/pages/OwnerBookings.jsx
+++ owner/src/pages/OwnerBookings.jsx
@@ -237,7 +237,7 @@ export default function OwnerBookings() {
                         {b.paymentStatus === "paid" ? (
                           <>
                             <DropdownMenuItem
-                              onSelect={() => window.open(`/owner/invoice/${b._id}`, "_blank")}
+                              onSelect={() => window.open(`/owner/invoice/${b._id}`,)}
                             >
                               View Invoice
                             </DropdownMenuItem>
