d58863e4a4a7bf647f8decfba8791ebe77159bb5
SakshiBandichawde <sakshibandichawade@gmail.com>
2025-12-23 14:58:42 +0530

    updated file

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoute.js";
 import propertyRoutes from "./routes/propertyRoute.js";
@@ -11,12 +14,24 @@ import adminRoutes from "./routes/admin.js";
 import ownerRoutes from "./routes/owner.routes.js";
 import locationRoutes from "./routes/locationRoutes.js";
 
-
 dotenv.config();
 const app = express();
 connectDB();
 
-const allowedOrigin = ["http://localhost:5173", "http://localhost:5174", "http://localhost:5175","https://karabookfrontend.netlify.app", "http://88.222.244.26/admin", "http://88.222.244.26", "https://karabookdev.cloud", "https://karabookdev.cloud/admin"];
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const allowedOrigin = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "http://localhost:5175",
+  "https://karabookfrontend.netlify.app",
+  "http://88.222.244.26/admin",
+  "http://88.222.244.26",
+  "https://karabookdev.cloud",
+  "https://karabookdev.cloud/admin",
+];
+
 app.use(
   cors({
     origin: function (origin, callback) {
@@ -35,7 +50,11 @@ app.use(
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
-app.use("/uploads", express.static("uploads"));
+app.use(
+  "/uploads",
+  express.static(path.join(__dirname, "uploads"))
+);
+
 app.get("/", (_req, res) => res.send("API is up"));
 
 app.use("/api/auth", userRoutes);
@@ -47,8 +66,7 @@ app.use("/api/admin", adminRoutes);
 app.use("/api/owner", ownerRoutes);
 app.use("/api", locationRoutes);
 
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
