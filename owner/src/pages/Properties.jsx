11423693e8604627732c7f3a94d2b67441d784c0
SakshiBandichawde <sakshibandichawade@gmail.com>
2025-12-27 21:43:14 +0530

    updated type

--- owner/src/pages/Properties.jsx
+++ owner/src/pages/Properties.jsx
@@ -62,6 +62,7 @@ export default function Properties() {
     extraAdultCharge,
     extraChildCharge,
     publishNow,
+    propertyType,
     coverImage,
     galleryPhotos = [],
     amenities = [],
