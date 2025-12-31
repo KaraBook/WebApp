23752302c736a146eee55af2e7defd76dd34d8a0
SakshiBandichawde <sakshibandichawade@gmail.com>
2025-12-31 17:24:29 +0530

    updated file

--- owner/src/components/AmenitiesAccordion.jsx
+++ owner/src/components/AmenitiesAccordion.jsx
@@ -26,7 +26,7 @@ export default function AmenitiesAccordion({
   };
 
   return (
-    <div className="flex flex-wrap items-start justify-between gap-4">
+    <div className="flex flex-wrap items-start justify-between gap-2">
       {options.map((group) => {
         const isOpen = openKeys.includes(group.key);
 
