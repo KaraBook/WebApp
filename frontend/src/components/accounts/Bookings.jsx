1e90e9a00a38bfe88a7ac4a7c8815c0012c0e6ae
SakshiBandichawde <sakshibandichawade@gmail.com>
2026-01-11 14:32:56 +0530

    updated file

--- frontend/src/components/accounts/Bookings.jsx
+++ frontend/src/components/accounts/Bookings.jsx
@@ -78,7 +78,7 @@ export default function Bookings() {
   };
 
   return (
-    <div className="w-full px-4 min-h-[calc(100vh-160px)]">
+    <div className="w-full px-2 md:px-4 min-h-[calc(100vh-160px)]">
       {/* TITLE */}
       <h1 className="text-2xl font-[500] uppercase tracking-[1px] text-[#233b19] mb-6">My Bookings</h1>
 
