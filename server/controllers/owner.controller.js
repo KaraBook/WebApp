8c09cf8303bb6e89195dccc9b07e15602e703b13
SakshiBandichawde <sakshibandichawade@gmail.com>
2026-01-12 15:27:51 +0530

    update file

--- server/controllers/owner.controller.js
+++ server/controllers/owner.controller.js
@@ -32,7 +32,7 @@ export const getOwnerDashboard = async (req, res) => {
     const propertyIds = properties.map((p) => p._id);
 
     const bookings = await Booking.find({ propertyId: { $in: propertyIds } })
-      .populate("userId", "firstName lastName mobile")
+      .populate("userId", "firstName lastName email mobile")
       .populate("propertyId", "propertyName");
 
     const uniqueUserIds = new Set(
@@ -95,7 +95,7 @@ export const getOwnerBookings = async (req, res) => {
 
     const bookings = await Booking.find({ propertyId: { $in: propertyIds } })
       .populate("propertyId", "propertyName")
-      .populate("userId", "firstName lastName mobile");
+      .populate("userId", "firstName lastName email mobile");
 
     res.json({ success: true, data: bookings });
   } catch (err) {
