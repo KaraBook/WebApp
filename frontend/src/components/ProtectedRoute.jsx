7cb90a890356ea078b6d095aa1f8eb45e71ce3b8
SakshiBandichawde <sakshibandichawade@gmail.com>
2026-01-21 22:07:48 +0530

    updated file

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -3,16 +3,7 @@ import { useAuthStore } from "@/store/auth";
 
 export default function ProtectedRoute() {
   const user = useAuthStore((s) => s.user);
-  const hasHydrated = useAuthStore.persist.hasHydrated();
   const loc = useLocation();
-
-  if (!hasHydrated) {
-    return null; 
-  }
-
-  if (!user) {
-    return <Navigate to="/" state={{ from: loc }} replace />;
-  }
-
+  if (!user) return <Navigate to="/" state={{ from: loc }} replace />;
   return <Outlet />;
 }
