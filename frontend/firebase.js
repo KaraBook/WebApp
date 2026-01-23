cf5f9362c2cb49858e42c288b64f8e68339629b6
SakshiBandichawde <sakshibandichawade@gmail.com>
2026-01-23 21:53:55 +0530

    updated file

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -12,29 +12,5 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
 
-export const buildRecaptcha = () => {
-  if (typeof window === "undefined") return null;
-
-  if (window.recaptchaVerifier) {
-    try { window.recaptchaVerifier.clear(); } catch {}
-    window.recaptchaVerifier = null;
-  }
-  window.recaptchaVerifier = new RecaptchaVerifier(
-    auth,
-    "recaptcha-container",
-    {
-      size: "invisible",
-      callback: () => console.log("reCAPTCHA verified"),
-      "expired-callback": () => {
-        console.log("Recaptcha expired");
-        window.recaptchaVerifier = null;
-      }
-    }
-  );
-  window.recaptchaVerifier.render();
-  return window.recaptchaVerifier;
-};
-
-
 
 export { signInWithPhoneNumber };
