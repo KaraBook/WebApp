134587f6b7f5bbe2d206f5d84efe75ab9e0163a6
SakshiBandichawde <sakshibandichawade@gmail.com>
2026-01-23 18:08:31 +0530

    up

--- frontend/src/components/BookingDetailsMobileDrawer.jsx
+++ frontend/src/components/BookingDetailsMobileDrawer.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { createPortal } from "react-dom";
 
 export default function BookingDetailsMobileDrawer({
