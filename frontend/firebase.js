3865f9668ab3ef07b8752563f78e5d3600559983
SakshiBandichawde <sakshibandichawade@gmail.com>
2026-01-24 10:50:21 +0530

    updated fire

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -10,6 +10,7 @@ const firebaseConfig = {
   authDomain: import.meta.env.VITE_FB_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FB_PROJECT_ID,
   appId: import.meta.env.VITE_FB_APP_ID,
+  messagingSenderId: import.meta.env.VITE_FB_MESSAGING_SENDER_ID,
 };
 
 const app = initializeApp(firebaseConfig);
