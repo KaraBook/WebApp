92834f3b5883a0db1a8e4fadd5caaefa029235f2
SakshiBandichawde <sakshibandichawade@gmail.com>
2026-02-08 20:41:01 +0530

    up

--- owner/src/pages/MyProfile.jsx
+++ owner/src/pages/MyProfile.jsx
@@ -339,6 +339,8 @@ export default function MyProfile() {
 
   /* ---------------- UI ---------------- */
   return (
+    <>
+    <div id="recaptcha-container"></div>
     <div className="max-w-4xl mx-auto p-4">
       <h1 className="text-[26px] font-bold mb-4">My Profile</h1>
 
@@ -670,6 +672,7 @@ export default function MyProfile() {
         </CardContent>
       </Card>
     </div>
+    </>
   );
 }
 
