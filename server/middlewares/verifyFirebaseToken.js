e458edea5b705793fb57b11572104ed94de3e1ff
SakshiBandichawde <sakshibandichawade@gmail.com>
2026-02-09 09:10:57 +0530

    up

--- server/middlewares/verifyFirebaseToken.js
+++ server/middlewares/verifyFirebaseToken.js
@@ -1,32 +1,19 @@
 import admin from "../config/firebase.js";
 
 export const verifyFirebaseToken = async (req, res, next) => {
-  let idToken = req.headers["x-firebase-token"];
-
-  if (!idToken) {
-    const authHeader = req.headers.authorization;
-
-    if (authHeader?.startsWith("Firebase ")) {
-      idToken = authHeader.split(" ")[1];
-    }
+  const authHeader = req.headers.authorization;
+  if (!authHeader?.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "No token provided" });
   }
 
-  if (!idToken) {
-    return res.status(401).json({
-      message: "Firebase token missing",
-    });
-  }
+  const idToken = authHeader.split(" ")[1];
 
   try {
     const decodedToken = await admin.auth().verifyIdToken(idToken);
-
     req.firebaseUser = decodedToken;
     next();
   } catch (error) {
     console.error("Firebase token verification failed:", error);
-
-    return res.status(401).json({
-      message: "Invalid Firebase token",
-    });
+    res.status(401).json({ message: "Invalid Firebase token" });
   }
 };
