6dc8be76d844c06bdd219f5db618a2f98b5ac43d
SakshiBandichawde <sakshibandichawade@gmail.com>
2026-02-15 15:12:21 +0530

    up

--- server/server.js
+++ server/server.js
@@ -51,7 +51,7 @@ app.use(
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
-const UPLOADS_PATH = path.join(process.cwd(), "server", "uploads");
+const UPLOADS_PATH = path.join(__dirname, "uploads");
 
 app.use(
   "/uploads",
