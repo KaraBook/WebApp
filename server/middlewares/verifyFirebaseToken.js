fcce310bacbbba14a794c48366e3a9d95a21fc51
SakshiBandichawde <sakshibandichawade@gmail.com>
2026-02-24 17:44:59 +0530

    up

--- server/middlewares/verifyFirebaseToken.js
+++ server/middlewares/verifyFirebaseToken.js
@@ -3,6 +3,7 @@ import admin from "../config/firebase.js";
 export const verifyFirebaseToken = async (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (!authHeader?.startsWith("Bearer ")) {
+     console.log("❌ No Authorization header");
     return res.status(401).json({ message: "No token provided" });
   }
 
@@ -10,9 +11,15 @@ export const verifyFirebaseToken = async (req, res, next) => {
 
   try {
     const decodedToken = await admin.auth().verifyIdToken(idToken);
+
+    console.log("✅ Firebase token verified");
+    console.log("Firebase UID:", decodedToken.uid);
+    console.log("Firebase Phone:", decodedToken.phone_number);
+    console.log("Firebase Provider:", decodedToken.firebase?.sign_in_provider);
     req.firebaseUser = decodedToken;
     next();
   } catch (error) {
+    console.log("❌ Firebase verify FAILED");
     console.error("Firebase token verification failed:", error);
     res.status(401).json({ message: "Invalid Firebase token" });
   }
