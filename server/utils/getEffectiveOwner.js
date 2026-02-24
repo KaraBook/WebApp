603c704ffb9917b22a1267b48f3034093bbd17bb
SakshiBandichawde <sakshibandichawade@gmail.com>
2026-02-24 20:56:50 +0530

    u

--- server/utils/getEffectiveOwner.js
+++ server/utils/getEffectiveOwner.js
@@ -1,9 +1,21 @@
 import User from "../models/User.js";
 
 export const getEffectiveOwnerId = async (req) => {
-  if (req.user.role === "manager") {
-    const manager = await User.findById(req.user.id).select("createdBy");
-    return manager.createdBy; 
+  const userId = req.user?.id || req.user?._id;
+
+  if (!userId) throw new Error("Unauthenticated");
+
+  const user = await User.findById(userId).select("roles primaryRole createdBy");
+
+  if (!user) throw new Error("User not found");
+
+  const roles = Array.isArray(user.roles)
+    ? user.roles
+    : (user.primaryRole ? [user.primaryRole] : []);
+
+  if (roles.includes("manager") && user.createdBy) {
+    return user.createdBy;
   }
-  return req.user.id; 
+
+  return user._id;
 };
