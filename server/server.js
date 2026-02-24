949ff4593f32127f4bbf2670548c5b03c7d17b74
SakshiBandichawde <sakshibandichawade@gmail.com>
2026-02-24 15:45:20 +0530

    up

--- server/server.js
+++ server/server.js
@@ -46,12 +46,8 @@ app.use(
       }
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
-app.options("*", cors());
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
